cd076da80f2ad0ed2d232143000e05b98954a488
yasminebenmouhamed <165170307+yasminebenmouhamed@users.noreply.github.com>
2025-06-09 10:23:55 +0100

    Delete api.js

--- wewantwaste-skip-selector/src/api.js
+++
@@ -1,7 +0,0 @@
-export const fetchSkips = async () => {
-  const response = await fetch(
-    "https://app.wewantwaste.co.uk/api/skips/by-location?postcode=NR32&area=Lowestoft"
-  );
-  const data = await response.json();
-  return data;
-};
